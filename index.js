ebb08b8ed56b0bb96f913778c0333c23f288d914
Yassine <yassine.bousaidi@hotmail.com>
2019-11-20 23:29:56 +0100

    'test'

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
  });
- 
+
 app.use(express.static('public'));
 app.use('/tweeter/',sentimentTweeter);
 app.use('/meteo/',weather);
@@ -20,6 +20,6 @@ app.use('/faceRecognition/',face);
 
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
 })
