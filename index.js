54f5b91f0431100ab99cfa679918677510c4230f
Yassine <yassine.bousaidi@hotmail.com>
2019-11-20 23:16:35 +0100

    'test'

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ let weather = (require('./weather'))
 var sentimentTweeter = require('./sentimentTweeter');
 var face = require('./face');
 
-
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+ 
 app.use(express.static('public'));
 app.use('/tweeter/',sentimentTweeter);
 app.use('/meteo/',weather);
