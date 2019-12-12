3e7afa866f5c9db2a14ee2a03b60edb137c985c8
Yassine <yassine.bousaidi@hotmail.com>
2019-12-12 03:19:23 +0100

    Modification de la recuperation des tweets

--- firstPage.js
+++ firstPage.js
@@ -6,21 +6,27 @@ var Twitter = require('twitter');
 
 
 var client = new Twitter({
-    consumer_key: 'E0OBpgATZe8yQOpGM7AEXApYQ',
-    consumer_secret: 'UzwRYSyYeCQjt2HcygqkNdSgPsbhT840pIRxiXla0zYjgNjotv',
-    access_token_key: '1147655039990865920-IzaPdTn87D7K8MbvQl4fRMCke1hRK4',
-    access_token_secret: 'OZLEuUqgpaaURK3XQERJku9M1Yzkp74MuZwfVVDFGRNqc'
+    consumer_key: 'KqowUo5o5SYTwBxGJMiIGHwcc',
+    consumer_secret: 'VXdfX4Ug8lfLx2dTVcFCQsQsROLcfvQhk64dybt2umrdAtAomH',
+    access_token_key: '841426149012045827-dVVEbUFxXwEQi0isZ4nHZ31YxLquxns',
+    access_token_secret: 'AimVu9AKQ8FJuS2KvIDk5TW6kwcXNPRdtnSpj8h8mc1eP'
   });
 // define the home page route
 router.get('/',  function(req, res) {
-  
-    var params = {tweet_mode: "extended", screen_name: "Quotes_PS", include_rts: 1 ,exclude_replies: true,is_quote_status:false, count: 200 };
+    myIP = req.connection.remoteAddress
+    var params = {tweet_mode: "extended", screen_name: "Quotes_PS", include_rts: 1 ,exclude_replies: true,is_quote_status:false, count: 3200 };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
+    
+
       if (!error) {
         var rand = tweets[Math.floor(Math.random() * tweets.length)];
         let txt = rand.full_text;
         console.log(rand);
-    
+        client.post('statuses/update', {status: "Eva viens de detecter la connexion d'une personne checker les logs pour en savoir plus."}, function(error, tweet, response) {
+            if (!error) {
+              console.log(tweet);
+            }
+        });
 
       res.render('firstPage', {mySentence :txt});
       }
