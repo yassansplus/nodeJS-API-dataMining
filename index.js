267b1bb695490edb8706f02a8f24de71f00532ae
Yassine <yassine.bousaidi@hotmail.com>
2019-12-12 17:25:33 +0100

    maj

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
 let indexRoute = require("./middleware/index");
-let weather = (require('./weather'))
-var sentimentTweeter = require('./sentimentTweeter');
-var face = require('./face');
-var firstPage = require('./firstPage');
-var sophia = require('./sophia')
+let weather = (require('./controller/weather'))
+var sentimentTweeter = require('./controller/sentimentTweeter');
+var face = require('./controller/face');
+var firstPage = require('./controller/firstPage');
+var sophia = require('./controller/sophia')
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 app.use('/tweeter/',sentimentTweeter);
--- middleware/Sqljob.js
+++ middleware/Sqljob.js
@@ -1,18 +1,68 @@
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "yourusername",
-    password: "yourpassword",
-    database: "mydb"
+// get the client
+const mysql = require('mysql2');
+ 
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'eva'
 });
+ 
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("1 record inserted");
-    });
-});
+function  CheckExistingInformation(tweet){
+    
+    let count =
+    connection.query(
+        'SELECT * FROM `news` WHERE `id` = ?',
+        [tweet.id],
+        function(err, results) {
+
+            return results.length;
+            
+        }
+      );
+      return count._rows.length;
+      
+     
+}
+
+
+
+function CheckExistingFact(tweet){
+    connection.query(
+        'SELECT count(*) FROM `fact` WHERE `id` = ?',
+        [tweet.id],
+        function(err, results) {
+          console.log(results);
+        }
+      );
+}
+
+function insertNews(tweet){
+    connection.execute(
+        'INSERT INTO news (id,userId,username,text) VALUES(?,?,?,?)',
+        [tweet.id,tweet.user.id,tweet.user.screen_name,tweet.full_text],
+        function(err, results) {
+            if(err){
+                console.log('ok');
+            }
+            console.log('\x1b[32m',"insertion du tweet")
+          console.log(results);
+        }
+      );
+
+    
+    //  connection.query(
+    //     'INSERT INTO news VALUES(?,?,?,?)',
+    //     [tweet.id,tweet.user.id,tweet.user.screen_name,tweet.full_text],
+    //     function(err, results, fields) {
+    //         console.log(err);
+    //       console.log(results); // results contains rows returned by server
+    //       console.log(fields); // fields contains extra meta data about results, if available
+       
+    //     }
+    //   );
+    
+}
+
+module.exports = {insertNews, CheckExistingInformation};
